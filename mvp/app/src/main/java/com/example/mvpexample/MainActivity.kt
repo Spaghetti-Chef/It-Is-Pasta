d2c9a58b612f0fce5fa8cfcdd618e7552c8c9d36
chaeyeong <shimcy2010@gmail.com>
2022-05-01 07:54:01 +0900

    view

--- mvp/app/src/main/java/com/example/mvpexample/MainActivity.kt
+++ mvp/app/src/main/java/com/example/mvpexample/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.mvpexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mvpexample.Presenter.ILoginPresenter
+import com.example.mvpexample.Presenter.LoginPresenter
+import com.example.mvpexample.View.ILoginView
+import com.example.mvpexample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ILoginView {
+    private lateinit var binding: ActivityMainBinding
+
+    internal lateinit var loginPresenter: ILoginPresenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Init
+        loginPresenter = LoginPresenter(this)
+
+        binding.btnLogin.setOnClickListener {
+            loginPresenter.onLogin(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+        }
+    }
+
+    override fun onLoginSuccess(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onLoginFail(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
